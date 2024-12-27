a28f6d653914be69ec7a3ff53ade688079683672
Jayashreesmitha <smitha4494@gamil.com>
2024-12-27 18:40:57 +0530

    terraform backend updated

--- Monolithic/frontend/backend.tf
+++ Monolithic/frontend/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-234567"
+    bucket  = "iac-terraform-123456"
     key     = "state/terraform.tfstate"
     region  = "us-east-2"
     encrypt = true
